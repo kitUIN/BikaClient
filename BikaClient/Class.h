fc7b3bf2b69471c5005e66cb4114cea933bf3bec
kitUIN <kulujun@gmail.com>
2022-05-17 14:42:13 +0800

    添加项目文件。

---
+++ BikaClient/Class.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::BikaClient::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::BikaClient::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
