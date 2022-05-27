44b83cb0c57878133335ab784b0c88ddd0841012
kitUIN <kulujun@gmail.com>
2022-05-27 22:11:53 +0800

    ActionResponse

---
+++ BikaClient/Responses/Responses.ActionResponse.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+#include "Responses.ActionResponse.h"
+#include "Responses.ActionResponse.g.cpp"
+
+
+namespace winrt::BikaClient::Responses::implementation
+{
+    ActionResponse::ActionResponse(winrt::Windows::Data::Json::JsonObject const& json)
+    {
+        m_json = json.Stringify();
+        if (json.HasKey(L"code")) m_code = static_cast<int32_t>(json.GetNamedNumber(L"code"));
+        if (json.HasKey(L"message")) m_message = json.GetNamedString(L"message");
+        if (json.HasKey(L"error")) m_error = json.GetNamedString(L"error");
+        if (json.HasKey(L"detail")) m_detail = json.GetNamedString(L"detail");
+        if (json.HasKey(L"data")) {
+            m_action = json.GetNamedObject(L"data").GetNamedString(L"action");
+        }
+    }
+    hstring ActionResponse::Action()
+    {
+        return m_action;
+    }
+    void ActionResponse::Action(hstring const& value)
+    {
+        m_action = value;
+    }
+    int32_t ActionResponse::Code()
+    {
+        return m_code;
+    }
+    void ActionResponse::Code(int32_t value)
+    {
+        m_code = value;
+    }
+    hstring ActionResponse::Message()
+    {
+        return m_message;
+    }
+    void ActionResponse::Message(hstring const& value)
+    {
+        m_message = value;
+    }
+    hstring ActionResponse::Error()
+    {
+        return m_error;
+    }
+    void ActionResponse::Error(hstring const& value)
+    {
+        m_error = value;
+    }
+    hstring ActionResponse::Detail()
+    {
+        return m_detail;
+    }
+    void ActionResponse::Detail(hstring const& value)
+    {
+        m_detail = value;
+    }
+    hstring ActionResponse::Json()
+    {
+        return m_json;
+    }
+}
---
+++ BikaClient/Responses/Responses.ActionResponse.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Responses.ActionResponse.g.h"
+
+
+namespace winrt::BikaClient::Responses::implementation
+{
+    struct ActionResponse : ActionResponseT<ActionResponse>
+    {
+        ActionResponse() = default;
+
+        ActionResponse(winrt::Windows::Data::Json::JsonObject const& json);
+        hstring Action();
+        void Action(hstring const& value);
+        int32_t Code();
+        void Code(int32_t value);
+        hstring Message();
+        void Message(hstring const& value);
+        hstring Error();
+        void Error(hstring const& value);
+        hstring Detail();
+        void Detail(hstring const& value);
+        hstring Json();
+    private:
+		hstring m_action{ L"" };
+        int32_t m_code{ 0 };
+        hstring m_message{ L"" };
+        hstring m_error{ L"" };
+        hstring m_detail{ L"" };
+        hstring m_json{ L"" };
+    };
+}
+namespace winrt::BikaClient::Responses::factory_implementation
+{
+    struct ActionResponse : ActionResponseT<ActionResponse, implementation::ActionResponse>
+    {
+    };
+}
