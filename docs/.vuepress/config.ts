26d19c56dc4f221fd2b48c75e3b7b1115ae74906
kitUIN <kulujun@gmail.com>
2022-07-30 17:21:27 +0800

    docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,7 @@ import theme from "./theme";
 export default defineUserConfig({
   lang: "zh-CN",
   title: "主题演示",
-  description: "vuepress-theme-hope 的演示",
+  description: "BikaClient的文档",
 
   base: "/",
 
